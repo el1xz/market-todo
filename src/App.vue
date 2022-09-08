19b9cc5b189b4a1b4a8ba052ca8f1354da158e5e
el1xz <glaiber2010@gmail.com>
2022-09-08 16:23:27 +0300

    todov3ls

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div class="flex items-center gap-4 ml-4 mt-4">
+    <div class="text-[14px] text-gray-600 cursor-pointer transition-all hover:border-gray-400 border rounded content px-2 py-1">
+    Sign in
+  </div>
+  <div class="text-[14px] text-gray-600 cursor-pointer transition-all hover:border-gray-400 border rounded content px-2 py-1">
+    Login
+  </div>
+  <input class="text-[14px] outline-none border transition-all rounded h-full px-2 py-[5px] focus:border-gray-500" type="text" placeholder="Введите данные" name="" id="">
+  </div>
+</template>
+
+<style>
+  .content {
+    width: fit-content;
+  }
+</style>
