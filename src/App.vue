cb0d7b1cf10a1190e7c39695e28b8853771f247b
el1xz <glaiber2010@gmail.com>
2022-09-09 13:06:09 +0300

    project update

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,30 @@
 </script>
 
 <template>
-  <div class="flex items-center gap-4 ml-4 mt-4">
-    <div class="text-[14px] text-gray-600 cursor-pointer transition-all hover:border-gray-400 border rounded content px-2 py-1">
-    Sign in
+  <div class="flex flex-col justify-between h-[100vh]">
+    <div class="flex items-center gap-4 ml-4 mt-4 main">
+    <div
+      class="text-[14px] text-gray-600 cursor-pointer transition-all hover:border-gray-400 border rounded content px-2 py-1">
+      Sign in
+    </div>
+    <div
+      class="text-[14px] text-gray-600 cursor-pointer transition-all hover:border-gray-400 border rounded content px-2 py-1">
+      Login
+    </div>
+    <input class="text-[14px] outline-none border transition-all rounded h-full px-2 py-[5px] focus:border-gray-500"
+      type="text" placeholder="Введите данные" name="" id="">
   </div>
-  <div class="text-[14px] text-gray-600 cursor-pointer transition-all hover:border-gray-400 border rounded content px-2 py-1">
-    Login
-  </div>
-  <input class="text-[14px] outline-none border transition-all rounded h-full px-2 py-[5px] focus:border-gray-500" type="text" placeholder="Введите данные" name="" id="">
+  <footer class="bg-gray-800 text-white footer py-4 px-4">
+    <div class="flex justify-between">
+      <a target=_blank class="text-gray-400 hover:text-white transition-all" href="https://www.youtube.com/watch?v=qhjxAP1hFuI">Video</a>
+      <a target=_blank class="text-gray-400 hover:text-white transition-all" href="https://github.com/el1xz">el1xz ©</a>
+    </div>
+  </footer>
   </div>
 </template>
 
 <style>
-  .content {
-    width: fit-content;
-  }
+.content {
+  width: fit-content;
+}
 </style>
