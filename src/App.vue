ca967dcf60f4970c2abc49f15dd1f919c4450771
el1xz <glaiber2010@gmail.com>
2022-09-15 12:50:02 +0300

    add functions

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,110 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// Импортируем нужные функции из Vue
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+// Метод sort() на месте сортирует элементы массива и возвращает отсортированный массив.
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}))
+
+const addTodo = () => {
+  // Проверка на пустые поля (пустое занчение value)
+  // Если правило соблюдается (пустое значение) ничего не возвращает, тем самым не добавляет задачу
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return
+  }
+  // Добавляем значение из поля ввода
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  })
+  // Обнуляем поле для ввода задачи 
+  input_content.value = ''
+  input_category.value = null
+}
+
+// Запись задач в localStorage
+// Метод JSON.stringify() преобразует значение JavaScript в строку JSON, возможно с заменой значений, если указана функция замены, или с включением только определённых свойств, если указан массив замены.
+watch(todos, newVal => {
+  localStorage.setItem('todos', JSON.stringify(newVal))
+},
+  { deep: true }
+)
+
+// Отслеживаем значение name с помощью функции watch и передаем данные в localStorage
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+// Получаем данные из localStorage с помощью хука onMounted
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+  // Метод JSON.parse() разбирает строку JSON, возможно с преобразованием получаемого в процессе разбора значения.
+  todos.value = JSON.parse(localStorage.getItem('todos')) || '[]'
+})
 </script>
 
 <template>
-  <div class="flex flex-col justify-between h-[100vh]">
-    <div class="flex items-center gap-4 ml-4 mt-4 main">
-    <div
-      class="text-[14px] text-gray-600 cursor-pointer transition-all hover:border-gray-400 border rounded content px-2 py-1">
-      Sign in
-    </div>
-    <div
-      class="text-[14px] text-gray-600 cursor-pointer transition-all hover:border-gray-400 border rounded content px-2 py-1">
-      Login
-    </div>
-    <input class="text-[14px] outline-none border transition-all rounded h-full px-2 py-[5px] focus:border-gray-500"
-      type="text" placeholder="Введите данные" name="" id="">
-  </div>
+  <main>
+    <section>
+      <h1>
+        Привет, <input type="text" placeholder="ваше имя" v-model="name">
+      </h1>
+    </section>
+
+    <section>
+      <h1>
+        Создать задачу
+      </h1>
+
+      <form @submit.prevent="addtodo">
+        <h1>
+          Что в вашем списке задач?
+        </h1>
+        <input type="text" placeholder="создать" v-model="input_content">
+
+        <h1>
+          Выбрать категорию
+        </h1>
+
+        <div>
+          <input type="radio" name="category" value="бизнес" v-model="input_category">
+          <div>бизнес</div>
+        </div>
+        <div>
+          <input type="radio" name="category" value="персональная" v-model="input_category">
+          <div>персональная</div>
+        </div>
+
+        <button @click="addTodo">
+          Добавить
+        </button>
+      </form>
+      <div>
+        <h1>
+          Список задач:
+        </h1>
+        <div v-for="todo in todos_asc">
+          <input type="text" v-model="todo.content">
+        </div>
+      </div>
+    </section>
+  </main>
   <footer class="bg-gray-800 text-white footer py-4 px-4">
     <div class="flex justify-between">
-      <a target=_blank class="text-gray-400 hover:text-white transition-all" href="https://www.youtube.com/watch?v=qhjxAP1hFuI">Video</a>
+      <a target=_blank class="text-gray-400 hover:text-white transition-all"
+        href="https://www.youtube.com/watch?v=qhjxAP1hFuI">Video</a>
       <a target=_blank class="text-gray-400 hover:text-white transition-all" href="https://github.com/el1xz">el1xz ©</a>
     </div>
   </footer>
-  </div>
 </template>
 
 <style>
