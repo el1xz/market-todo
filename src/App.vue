9111abb38d9b69412d5ba48a39a5cae07d225d5f
el1xz <glaiber2010@gmail.com>
2022-09-16 14:30:44 +0300

    update ui and logic

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,56 @@
-<template>
-  <div id="app" class="w-3/5 mx-auto p-4 bg-gray-800 rounded-md m-4 relative overflow-y-hidden pt-14 mt-20">
-    <div class="w-full absolute bg-purple-600 top-0 right-0 left-0 text-center text-white font-semibold text-xl py-2">
-      <h1>Manpage of Todo</h1>
-    </div>
-    <form @submit.prevent="addNewTodo()" class="border-b-2 border-purple-600 py-4">
-      <div class="flex items-center">
-        <label class="text-white text-lg font-semibold">Todo Name: </label>
-        <input v-model="newTodo" type="text" class="flex-1 px-2 py-1 bg-gray-300 rounded-sm text-gray-800">
-      </div>
-      <div class="mt-5 flex items-center justify-between">
-        <button
-          class="px-4 py-2 text-sm font-semibold text-white bg-purple-600 rounded-sm focus:outline-none hover:bg-purple-500 focus:ring-4 ring-purple-600 ring-opacity-25 transition duration-100 ease-in-out transform hover:scale-105"
-        >
-          Add Todo
-        </button>
-        <button
-          class="p-3 rounded-full focus:outline-none hover:bg-purple-600 hover:bg-opacity-25"
-          @click="markAllCompleted()"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current text-purple-600" viewBox="0 0 20 20">
-            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-          </svg>
-        </button>
-      </div>
-    </form>
+<!-- 
+  // Добавить emty list
+  //  
+-->
 
-    <div id="todos" class="space-y-4 my-4">
-      <div
-        id="todo"
-        v-for="todo in todos"
-        :key="todo.id"
-        @click="todoCompleted(todo)"
-        class="py-4 px-2 rounded-lg bg-gray-300 cursor-pointer transform hover:scale-105 transition duration-100 ease-in-out flex items-center justify-between"
-        :class="{'bg-purple-600 text-white line-through': todo.isCompleted}"
-      >
-        <h1>{{todo.text}}</h1>
-        <span
-          class="p-2 rounded-full cursor-pointer hover:bg-purple-700 hover:bg-opacity-20"
-          :class="{'hover:bg-white text-white hover:bg-opacity-25': !todo.isCompleted}"
-          @click="deleteTodo(todo)"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current" viewBox="0 0 20 20">
-            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-          </svg>
-        </span>
+<template>
+  <main class="mt-4">
+    <div class="max-w-[500px] border border-gray-200 bg-gray-100 m-auto py-4 px-8 rounded-xl">
+      <div>
+        <div>
+          <h1 class="text-[24px] font-extrabold text-gray-500">MARKET LIST</h1>
+        </div>
+
+        <form @submit.prevent="addNewTodo()">
+          <h1 class="text-[18px] mt-4 text-gray-500">
+            Add product
+          </h1>
+          <input v-model="newTodo" class="text-[16px] px-2 py-1 rounded border outline-none" type="text">
+          <button
+            class="ml-4 bg-gray-300 hover:bg-gray-400 transition-all px-2 py-1 rounded text-white border border-gray-300">
+            ADD
+          </button>
+          <button
+            class="ml-4 bg-red-300 hover:bg-red-400 transition-all px-2 py-1 rounded text-white border border-red-300">
+            CLEAR
+          </button>
+          <button
+            @click="markAllCompleted()"
+            class="ml-4 bg-blue-300 hover:bg-blue-400 transition-all px-2 py-1 rounded text-white border border-blue-300">
+            DO ALL
+          </button>
+        </form>
+      </div>
+
+      <div class="border rounded my-4">
+      </div>
+
+      <div>
+        <div v-for="todo in todos" :key="todo.id" @click="todoCompleted(todo)" class="bg-gray-200 px-4 py-1 text-[18px] rounded border border-gray-300 flex justify-between items-center my-4">
+          <p :class="{'line-through': todo.isCompleted}">
+            {{todo.text}}
+          </p>
+          <div @click="deleteTodo(todo)" class="bg-gray-300 border border-gray-400 cursor-pointer px-2 py-1 rounded">
+            X
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
-import {ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 
 export default {
   name: 'App',
@@ -58,7 +58,7 @@ export default {
     const newTodo = ref('')
     const todos = ref([])
 
-    onMounted(()=> {
+    onMounted(() => {
       if (localStorage.getItem('manTodos')) {
         let manTodos = JSON.parse(localStorage.getItem('manTodos'))
         manTodos.forEach(todo => {
@@ -87,14 +87,14 @@ export default {
     }
 
     function deleteTodo(todo) {
-      todos.value = todos.value.filter( (item) => {
+      todos.value = todos.value.filter((item) => {
         return item != todo
       })
       localStorage.setItem('manTodos', JSON.stringify(todos.value))
     }
 
     function markAllCompleted() {
-      todos.value.forEach( (todo) => {
+      todos.value.forEach((todo) => {
         todo.isCompleted = true
       })
       localStorage.setItem('manTodos', JSON.stringify(todos.value))
@@ -114,12 +114,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
 * {
-  font-family: 'Anonymous Pro';
+  font-family: 'Karla', sans-serif;
 }
 </style>
