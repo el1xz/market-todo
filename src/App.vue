f2ec55d12a5dfe713f3072b1d1d18205be947ec4
el1xz <glaiber2010@gmail.com>
2022-09-19 16:59:35 +0300

    icon / ux / todo updates

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,51 @@
 <!-- 
-  // Добавить emty list
-  //  Добавить progress-bar
+  // Добавить progress-bar
+  // Добавить дату создания задачи
+  // Добавить темную тему
+  // Изменить UX
+  // No Task | Emty
 -->
 
 <template>
   <main class="mt-4">
-    <div class="max-w-[500px] border border-gray-200 bg-gray-100 m-auto py-4 px-8 rounded-xl">
+    <div class="max-w-[400px] min-h-[400px] border border-gray-200 bg-white m-auto py-4 px-8 rounded-xl">
       <div>
         <div>
-          <h1 class="text-[24px] font-extrabold text-gray-500">MARKET LIST</h1>
+          <h1 class="text-[24px] font-extrabold text-gray-500">Todo App</h1>
         </div>
 
         <form @submit.prevent="addNewTodo()">
           <h1 class="text-[18px] mt-4 text-gray-500">
-            Add product {{localeTime}} {{localeDate}}
+            {{localeDate}} {{localeTime}}
           </h1>
-          <input v-model="newTodo" class="text-[16px] px-2 py-1 rounded border outline-none" type="text">
-          <button
-            class="ml-4 bg-gray-300 hover:bg-gray-400 transition-all px-2 py-1 rounded text-white border border-gray-300">
-            ADD
-          </button>
-          <button
+          <div class="flex mt-2">
+            <input v-model="newTodo" class="text-[16px] w-full px-2 py-1 rounded-l-lg border-[2px] outline-none" placeholder="New Task" type="text">
+            <button
+              class="bg-[#704bd6] hover:bg-[#5b3cb0] transition-all px-6 py-2 rounded-r-lg text-white">
+              Add
+            </button>
+          </div>
+          <!-- <button
             class="ml-4 bg-red-300 hover:bg-red-400 transition-all px-2 py-1 rounded text-white border border-red-300">
             CLEAR
           </button>
           <button @click="markAllCompleted()"
             class="ml-4 bg-blue-300 hover:bg-blue-400 transition-all px-2 py-1 rounded text-white border border-blue-300">
             DO ALL
-          </button>
+          </button> -->
         </form>
       </div>
 
-      <div class="border rounded my-4">
-      </div>
-
       <div>
+        <div class="text-center font-medium text-[26px] text-gray-200 mt-[30%]" v-show="todos.length < 1">
+          No Task
+        </div>
         <div v-for="todo in todos" :key="todo.id" @click="todoCompleted(todo)"
-          class="bg-gray-200 px-4 py-1 text-[18px] rounded border border-gray-300 flex justify-between items-center my-4">
+          class="bg-gray-50 px-4 py-2 text-[18px] rounded shadow flex justify-between items-center my-4">
           <p :class="{'line-through': todo.isCompleted}">
             {{todo.text}}
           </p>
-          <div @click="deleteTodo(todo)" class="bg-gray-300 border border-gray-400 cursor-pointer px-2 py-1 rounded">
+          <div @click="deleteTodo(todo)" class="bg-[#704bd6] text-[14px] border text-white cursor-pointer px-3 py-1 rounded-full">
             X
           </div>
         </div>
