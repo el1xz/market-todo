049c9f557f81c39dbd12c0f45d568ec122dc7c62
el1xz <glaiber2010@gmail.com>
2022-09-19 11:50:32 +0300

    add current date and time

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!-- 
   // Добавить emty list
-  //  
+  //  Добавить progress-bar
 -->
 
 <template>
@@ -13,7 +13,7 @@
 
         <form @submit.prevent="addNewTodo()">
           <h1 class="text-[18px] mt-4 text-gray-500">
-            Add product
+            Add product {{localeTime}} {{localeDate}}
           </h1>
           <input v-model="newTodo" class="text-[16px] px-2 py-1 rounded border outline-none" type="text">
           <button
@@ -24,8 +24,7 @@
             class="ml-4 bg-red-300 hover:bg-red-400 transition-all px-2 py-1 rounded text-white border border-red-300">
             CLEAR
           </button>
-          <button
-            @click="markAllCompleted()"
+          <button @click="markAllCompleted()"
             class="ml-4 bg-blue-300 hover:bg-blue-400 transition-all px-2 py-1 rounded text-white border border-blue-300">
             DO ALL
           </button>
@@ -36,7 +35,8 @@
       </div>
 
       <div>
-        <div v-for="todo in todos" :key="todo.id" @click="todoCompleted(todo)" class="bg-gray-200 px-4 py-1 text-[18px] rounded border border-gray-300 flex justify-between items-center my-4">
+        <div v-for="todo in todos" :key="todo.id" @click="todoCompleted(todo)"
+          class="bg-gray-200 px-4 py-1 text-[18px] rounded border border-gray-300 flex justify-between items-center my-4">
           <p :class="{'line-through': todo.isCompleted}">
             {{todo.text}}
           </p>
@@ -50,14 +50,36 @@
 </template>
 
 <script>
+// Импортируем ref и хук жизенного цикла onMounted из библиотеки vue
+// Вызывается после того, как экземпляр компонента размонтирован. 
+// Когда этот хук вызван, все директивы экземпляра компонента уже отвязаны, слушатели событий удалены, дочерние экземпляры размонтированы.
 import { ref, onMounted } from 'vue'
 
 export default {
   name: 'App',
+  data: () => ({
+    date: '',
+    time: '',
+  }),
+  // Автообновление данных времени и даты
+  created() {
+    this.intervalId = setInterval(() => this.time = Date.now(), 1000);
+    this.intervalId = setInterval(() => this.date = Date.now(), 1000);
+  },
+  // Получаем данные о дате и времени и обязятально делаем метод toLocaleDateString() (возвращает строку с языкозависимым представлением части с датой в этой дате.)
+  computed: {
+    localeDate() {
+      return (new Date(this.date)).toLocaleDateString()
+    },
+    localeTime() {
+      return (new Date(this.time)).toLocaleTimeString()
+    },
+  },
   setup() {
     const newTodo = ref('')
     const todos = ref([])
 
+    // С помощью хука жизенного цикла записываем данные localStorage и пушим данные в список
     onMounted(() => {
       if (localStorage.getItem('manTodos')) {
         let manTodos = JSON.parse(localStorage.getItem('manTodos'))
@@ -66,6 +88,8 @@ export default {
         });
       }
     })
+
+    // Функция добавления новой задачи и записи в localStorage
     function addNewTodo() {
       // console.log(newTodo.value);
       if (newTodo.value.trim()) {
@@ -81,11 +105,14 @@ export default {
       }
     }
 
+    // Функция выполения задачи и записи в lS
+    // Метод JSON.stringify()преобразует значение JavaScript в строку JSON, дополнительно заменяя значения, если указана функция-заменитель, или дополнительно включая только указанные свойства, если указан массив-заменитель.
     function todoCompleted(todo) {
       todo.isCompleted = !todo.isCompleted
       localStorage.setItem('manTodos', JSON.stringify(todos.value))
     }
 
+    // Функция удаления и запись в lS
     function deleteTodo(todo) {
       todos.value = todos.value.filter((item) => {
         return item != todo
@@ -93,6 +120,7 @@ export default {
       localStorage.setItem('manTodos', JSON.stringify(todos.value))
     }
 
+    // Функция выполение всех задач
     function markAllCompleted() {
       todos.value.forEach((todo) => {
         todo.isCompleted = true
@@ -100,6 +128,8 @@ export default {
       localStorage.setItem('manTodos', JSON.stringify(todos.value))
     }
 
+
+    // Возвращаем из функции 
     return {
       newTodo,
       todos,
